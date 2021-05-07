85309d765ec581f54badaa2deca00ecbaeeaa681
Akshay171717 <62885718+Akshay171717@users.noreply.github.com>
2021-05-07 10:22:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import WriteStory from "./Screens/WriteStory"
+import ReadStory from "./Screens/ReadStory"
+import {createAppContainer} from "react-navigation"
+ import { createBottomTabNavigator } from "react-navigation-tabs"
+
+export default class App extends React.Component {
+  render(){
+    return (
+     <Tab/>
+    );
+  }
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+const Tab = createBottomTabNavigator({
+  WriteStory:WriteStory, ReadStory:ReadStory
+});
+const AppContainter = createAppContainter(Tab)
